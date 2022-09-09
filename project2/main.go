609404ee5c3da0f79ee21a9794724d612d116d15
benjark <brkloepfer@gmail.com>
2022-09-09 13:26:45 -0600

    Shuffling a random array

--- project2/main.go
+++ project2/main.go
@@ -5,98 +5,10 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"os"
+	"time"
 )
-
-// package main
-
-// import (
-// 	"encoding/csv"
-// 	"fmt"
-// 	"io"
-// 	"log"
-// 	"os"
-// )
-
-// func main() {
-
-// 	f, err := os.Open("names.csv")
-
-// 	if err != nil {
-
-// 		log.Fatal(err)
-// 	}
-
-// 	r := csv.NewReader(f)
-
-// 	for {
-
-// 		record, err := r.Read()
-
-// 		if err == io.EOF {
-// 			break
-// 		}
-
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-
-// 		for value := range record {
-// 			fmt.Printf("%s\n", record[value])
-// 		}
-// 	}
-// }
-
-// func readCsvFile(names.csv string) [][]string {
-// 	f, err := os.Open(names.csv)
-// 	if err != nil {
-// 		log.Fatal("Unable to read input file "+names.csv, err)
-// 	}
-// 	defer f.Close()
-
-// 	csvReader := csv.NewReader(f)
-// 	records, err := csvReader.ReadAll()
-// 	if err != nil {
-// 		log.Fatal("Unable to parse file as CSV for "+names.csv, err)
-// 	}
-
-// 	return records
-// }
-
-// func main() {
-// 	records := readCsvFile("names.csv")
-// 	fmt.Println(records)
-// }
-
-// func parseLocation(names.csv string) (map[string]*Point, error) {
-//     f, err := os.Open(names.csv)
-//     if err != nil {
-//         return nil, err
-//     }
-//     defer f.Close()
-
-//     csvr := csv.NewReader(f)
-
-//     locations := map[string]*Point{}
-//     for {
-//         row, err := csvr.Read()
-//         if err != nil {
-//             if err == io.EOF {
-//                 err = nil
-//             }
-//             return locations, err
-//         }
-
-//         p := &Point{}
-//         if p.lat, err = strconv.ParseFloat(row[1], 64); err != nil {
-//             return nil, err
-//         }
-//         if p.lon, err = strconv.ParseFloat(row[2], 64); err != nil {
-//             return nil, err
-//         }
-//         locations[row[0]] = p
-//     }
-// }
 
 func main() {
 	// open file
@@ -123,5 +35,12 @@ func main() {
 		// do something with read line
 		fmt.Printf("%+v\n", rec)
 	}
+
+	rand.Seed(time.Now().UnixNano())
+	names := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	rand.Shuffle(len(names), func(i, j int) {
+		names[i], names[j] = names[j], names[i]
+	})
+	fmt.Println(names)
 
 }
